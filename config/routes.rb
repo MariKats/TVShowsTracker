8859c626493aebecd7845e3537191fa34ea709f3
Niky Morgan <nicole.morgan@gmail.com>
2017-06-30 11:29:20 -0400

    Scope down routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      #scope down to only resources you need
       resources :shows
       resources :seasons
       post '/auth', to: 'auth#create'
